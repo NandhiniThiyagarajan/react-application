33d328c55b89fe6972e3722896176d9d8cda8991
Rafiu-tharaja <rafizahir1932001@gmail.com>
2024-08-12 22:15:46 +0530

    irritating this

--- src/Thara.js
+++ src/Thara.js
@@ -6,21 +6,7 @@ function Thara() {
     console.log("rafiiiiiiiiiiiii");
   };
 
-  return (
-    <div className="App">
-      hello
-      <div>Thara</div>
-      <button onClick={handleClick}>click me</button>
-      <div>Thara rafiu is an intelligent girl</div>
-      <div>this is so irritating</div>
-      <div> in every hardship, there is a relief</div>
-      <div>i m uwdhuqwhku</div>
-      <div>hiiii</div>
-      <div>Thara rafiu is an intelligent girl</div>
-      <div>Thara rafiu is an intelligent girl</div>
-      <div>Thara rafiu is an intelligent girl</div>
-    </div>
-  );
+  return <div className="App">hiiiiiiiiiiiiiiiiiiiiiiiiii</div>;
 }
 
 export default Thara;
