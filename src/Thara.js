429d555377e274852c164c852f4d2de063bc5fb8
NandhiniThiyagarajan <nandhini93448@gmail.com>
2024-08-12 22:10:34 +0530

    removed

--- src/Thara.js
+++ src/Thara.js
@@ -9,18 +9,7 @@ function Thara() {
   return (
     <div className="App">
       hello
-      <div>Thara</div>
-      <button onClick={handleClick}>click me</button>
-      <div>Thara rafiu is an intelligent girl</div>
-      <div>this is so irritating</div>
-      <div> in every hardship, there is a relief</div>
-      <div>i m uwdhuqwhku</div>
-      <div>hiiii</div>
-      <div>Thara rafiu is an intelligent girl</div>
-      <div>Thara rafiu is an intelligent girl</div>
-      <div>Thara rafiu is an intelligent girl</div>
-<div>hiiiiiiiii</div>
-<div>hello</div>
+    
 
   
     </div>
