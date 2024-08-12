961bf123ad231e1bff56d028475200b808aa3d66
Rafiu-tharaja <rafizahir1932001@gmail.com>
2024-08-12 21:50:05 +0530

    added a button in thara

--- src/Thara.js
+++ src/Thara.js
@@ -2,10 +2,15 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function Thara() {
+  const handleClick = () => {
+    console.log("rafiiiiiiiiiiiii");
+  };
+
   return (
     <div className="App">
       hello
       <div>Thara</div>
+      <button onClick={handleClick}>click me</button>
     </div>
   );
 }
